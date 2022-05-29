7d4965d2ac8c4e0faa68179d16b78465cef964c5
TheWiseWolfy <bogdan_gabriel757@yahoo.com>
2022-05-29 13:10:05 +0300

    Finished the final test.

--- TestareOLX/TestareOLX/FavoriteTest.cs
+++ TestareOLX/TestareOLX/FavoriteTest.cs
@@ -26,36 +26,26 @@ namespace TestareOLX
             //Wait for the page to load
             Thread.Sleep(1000);
 
-
-            var firstOffer = driver.FindElement(By.CssSelector(".gallerywide > :first-child"));
-
-            var offerNameElement = firstOffer.FindElement(By.CssSelector(".gallerywide > :first-child .inner a"));
-            string offerName = offerNameElement.FindElement(By.CssSelector(".gallerywide > :first-child  > .inner strong")).Text;
-
-            Console.WriteLine("Numele ofertei adaugate la favorite:");
-
-            Console.WriteLine(offerName);
-
-            var favoriteButton = firstOffer.FindElement(By.CssSelector(".favtab"));
-            favoriteButton.Click();
-
-            Thread.Sleep(1000);
-            var nuMultumesc = driver.FindElement(By.CssSelector("[data-cy=\"search_results_button_close_observed_search_info_message\"]"));
-            nuMultumesc.Click();
+            //Aici apasam pe inima si dam nu multumesc cand ne cere sa facem cont
+           var offerName = AdaugaLaFavorite(driver);
 
 
-         
-
+            //Prima oara cand dam click, butonul e ascuns, dar incercarea il forteaza sa drea scoll 
+            //pana sus si afiseaza bunonul dupa il putem apasa
             var acceseazaListaFavorite = driver.FindElement(By.Id("observed-ads-link"));
-			try { 
-            acceseazaListaFavorite.Click();
+
+            try
+            { 
+                acceseazaListaFavorite.Click();
+                Thread.Sleep(1000);
             }
-			catch (Exception e){}
-            Thread.Sleep(1000);
+            catch (Exception e){}
 
             acceseazaListaFavorite.Click();
-            Thread.Sleep(1000);
 
+
+            //Incarca pagina 
+            Thread.Sleep(1000);
 			//Garanteaza ca e o lista
 
 			try { 
@@ -73,6 +63,92 @@ namespace TestareOLX
             Assert.IsTrue((numeOfertaFavorita == offerName));
 
             driver.Close();
+        }
+
+        [TestMethod]
+        public void TestareStergereFavorite()
+		{
+            var driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://www.olx.ro/");
+
+
+            //Accept cookies
+            var cookieButton = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
+            cookieButton.Click();
+
+            //Wait for the page to load
+            Thread.Sleep(1000);
+
+            //Aici apasam pe inima si dam nu multumesc cand ne cere sa facem cont
+            var offerName = AdaugaLaFavorite(driver);
+
+            //Prima oara cand dam click, butonul e ascuns, dar incercarea il forteaza sa drea scoll 
+            //pana sus si afiseaza bunonul dupa il putem apasa
+            var acceseazaListaFavorite = driver.FindElement(By.Id("observed-ads-link"));
+
+            try
+            {
+                acceseazaListaFavorite.Click();
+                Thread.Sleep(1000);
+            }
+            catch (Exception e) { }
+
+            acceseazaListaFavorite.Click();
+
+            //Incarca pagina 
+            Thread.Sleep(1000);
+            //Garanteaza ca e o lista
+
+            try
+            {
+                driver.FindElement(By.Id("observedViewList")).Click();
+            }
+            catch (Exception e) { }
+            Thread.Sleep(1000);
+            //
+
+            var stergeDinFavorite = driver.FindElement(By.CssSelector(".removeObservedAd"));
+            stergeDinFavorite.Click();
+
+            Thread.Sleep(1000);
+
+            IWebElement ofertaFavorita = null;
+            try 
+            { 
+                ofertaFavorita = driver.FindElement(By.CssSelector(".offers td:first-child .title-cell span"));
+            }
+			catch(Exception e){}
+
+            Assert.IsNull(ofertaFavorita);
+
+            driver.Close();
+        }
+
+
+        public string AdaugaLaFavorite(ChromeDriver driver)
+		{
+            var firstOffer = driver.FindElement(By.CssSelector(".gallerywide > :first-child"));
+
+            var offerNameElement = firstOffer.FindElement(By.CssSelector(".gallerywide > :first-child .inner a"));
+            string offerName = offerNameElement.FindElement(By.CssSelector(".gallerywide > :first-child  > .inner strong")).Text;
+
+            Console.WriteLine("Numele ofertei adaugate la favorite:");
+
+            Console.WriteLine(offerName);
+
+            var favoriteButton = firstOffer.FindElement(By.CssSelector(".favtab"));
+            favoriteButton.Click();
+
+            Thread.Sleep(1000);
+
+            //Nu vrem sa ne facem cont
+            var nuMultumesc = driver.FindElement(By.CssSelector("[data-cy=\"search_results_button_close_observed_search_info_message\"]"));
+            nuMultumesc.Click();
+
+            return offerName;
         }
     }
 }
